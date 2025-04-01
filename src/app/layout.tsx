f0bada28be358e6aceb15097ea070e0b5f4f4da2
tekko <claireholmaert@gmail.com>
2025-04-01 15:22:35 +0200

    📦 NEW: favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr">
+      <link rel="shortcut icon" href="/svg/pokeball.svg" type="image/x-icon" />
       <body
       >
         {children}
